0eb67a9c4d9a74aaa0db0d77a9c6af882987d5ea
Barbara Whiting <whitingba@gmail.com>
2019-08-03 10:18:33 -0600

    working drink carousel with ingredients and instructions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,69 +7,78 @@ let drinkDetailsURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=
 
 //Generates a random number to select a random drink
 let getRandom = (function (maxNumber) {
-    return Math.floor(Math.random() * maxNumber);
- })
+  return Math.floor(Math.random() * maxNumber);
+})
 
 //Ajax call to fetch a list of drinks based on ingredient
 $.ajax({
-   url: drinkURL,
-   method: 'GET'
+  url: drinkURL,
+  method: 'GET'
 }).then(function (response) {
-    
 
-   for (var i = 0; i <= 5; i++) {
-        //Generates a random number on each loop and chooses a drink
-       let randomDrink = getRandom(response.drinks.length);
-       let currentDrink = response.drinks[randomDrink];
 
-       //Appends the current drink in to the corresponding span pre-set in HTML.
-       //Assigns a data-attr 'id' to be used as a unique identifier for second Ajax call.
-       $('#drinkName' + i).html(currentDrink.strDrink);
-       $('#drinkName' + i).attr('data-id', currentDrink.idDrink)
+  for (var i = 0; i <= 5; i++) {
+    //Generates a random number on each loop and chooses a drink
+    let randomDrink = getRandom(response.drinks.length);
+    let currentDrink = response.drinks[randomDrink];
 
-   }
-   //Calling this function after the above 'for loop' finishes to keep code synchronous
-   drinkId()
+
+    //Appends the current drink in to the corresponding span pre-set in HTML.
+    //Assigns a data-attr 'id' to be used as a unique identifier for second Ajax call.
+    $('#drinkName' + i).html(currentDrink.strDrink);
+    $('#drinkName' + i).attr('data-id', currentDrink.idDrink)
+
+  }
+  //Calling this function after the above 'for loop' finishes to keep code synchronous
+  drinkId()
 })
 
 
 //This function makes an Ajax call for each drink name added into the HTML
 function drinkId() {
 
-    for (let i = 1; i < 6; i++) {
-        //Pulls the data attr 'id' value from #drinkName span to use as 'key' for URL
-       var key = $('#drinkName' + i).attr('data-id')
-       
-       $.ajax({
-           url: drinkDetailsURL + key,
-           method: 'GET'
-       }).then(function (response) {
-            
-            //sets the corresponding img attr to the drinks url
-            var drinkImg = response.drinks[0].strDrinkThumb
-            $('#drinkImage' + i).attr('src', drinkImg)
-           
+  for (let i = 1; i < 6; i++) {
+    //Pulls the data attr 'id' value from #drinkName span to use as 'key' for URL
+    var key = $('#drinkName' + i).attr('data-id')
 
-       })
-   }
+    $.ajax({
+      url: drinkDetailsURL + key,
+      method: 'GET'
+    }).then(function (response) {
+
+      //sets the corresponding img attr to the drinks url
+      var currentDrinkDetail = response.drinks[0]
+      var drinkImg = response.drinks[0].strDrinkThumb
+      $('#drinkImage' + i).attr('src', drinkImg)
+      $('#drinkInstructions' + i).html(currentDrinkDetail.strInstructions);
+
+      //function call to bring in drink ingredients
+      parseIngredients(currentDrinkDetail, i);
+
+    })
+  }
 }
 
-//bring in ingredients
-// parseIngredients(currentDrinkDetail, i);
+
+
 
 //multiple ingredients function
-// let parseIngredients = (function (drink, drinkPosition) {
-//     for (i = 1; i <= 15; i++) {
-//         if (drink['strMeasure' + i] != null && drink['strIngredient' + i] != null) {
-//             $('#drinkIngredients1').append('<li>' + drink['strMeasure' + i] + ' ' + drink['strIngredient' + i] + '</li>');
-//         }
+let parseIngredients = (function (drink, drinkPosition) {
 
-//     }
-// })
 
-let getRandom = function(maxNumber) {
-  return Math.floor(Math.random() * maxNumber);
-};
+  for (let i = 1; i <= 15; i++) {
+
+    if (drink['strMeasure' + i] != null && drink['strIngredient' + i] != null) {
+      $('#drinkIngredients' + drinkPosition).append('<li>' + drink['strMeasure' + i] + ' ' + drink['strIngredient' + i] + '</li>');
+
+    }
+
+  }
+})
+
+// let getRandom = function (maxNumber) {
+//   return Math.floor(Math.random() * maxNumber);
+// };
 
 var lat;
 var lng;
@@ -104,7 +113,7 @@ function showPosition(position) {
     $.ajax({
       url: weatherURL,
       method: "GET"
-    }).then(function(weatherResponse) {
+    }).then(function (weatherResponse) {
       console.log(weatherResponse);
       // if(weatherResponse.)
 
@@ -120,8 +129,8 @@ function showPosition(position) {
       weatherImg.attr(
         "src",
         "http://openweathermap.org/img/wn/" +
-          weatherResponse.weather[0].icon +
-          "@2x.png"
+        weatherResponse.weather[0].icon +
+        "@2x.png"
       );
 
       var cardStack = $("<div>");
