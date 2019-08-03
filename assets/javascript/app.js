3e2d2e46ef1d3be0f8511a7cdad43d3ffdba9c47
Barbara Whiting <whitingba@gmail.com>
2019-08-03 17:35:22 -0600

    created the conditional based on the weather as well as created a function that would load the drinks based on the weather

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 ///////////////////////Global Variables//////////////////////////////////////////////
-var ingredient
+var ingredient;
 
 
 ///////////////////////Weather and Location Code/////////////////////////////////////
@@ -38,12 +38,7 @@ function showPosition(position) {
       method: "GET"
     }).then(function (weatherResponse) {
       console.log(weatherResponse);
-      var temp = weatherResponse.main.temp
-      if (temp > 65) {
-          ingredient = 'pineapple juice'
-      } else {
-          ingredient = 'coffee'
-      }
+
 
       $("#weatherDisplay").empty(); //emptying the weather display
 
@@ -93,12 +88,13 @@ function test() {
 //////////////////////////////////Cocktail Code///////////////////////////////////////////////////////
 
 
-//'Search by ingredient' API URL. Response contains drink name, drink id, and drink pic. 
-//Seach currently set to to global variable 'ingredient' determined by weather temperature.
-let drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
 
-//'Search by id' API URL. Response contains name, pic, measurements, instructions, and ingredients
-let drinkDetailsURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+
+
+
+
+
 
 
 //Generates a random number to select a random drink
@@ -106,28 +102,8 @@ let getRandom = (function (maxNumber) {
   return Math.floor(Math.random() * maxNumber);
 })
 
-//Ajax call to fetch a list of drinks based on ingredient
-$.ajax({
-  url: drinkURL,
-  method: 'GET'
-}).then(function (response) {
-
-
-  for (var i = 0; i <= 5; i++) {
-    //Generates a random number on each loop and chooses a drink
-    let randomDrink = getRandom(response.drinks.length);
-    let currentDrink = response.drinks[randomDrink];
-
-
-    //Appends the current drink in to the corresponding span pre-set in HTML.
-    //Assigns a data-attr 'id' to be used as a unique identifier for second Ajax call.
-    $('#drinkName' + i).html(currentDrink.strDrink);
-    $('#drinkName' + i).attr('data-id', currentDrink.idDrink)
-
-  }
-  //Calling this function after the above 'for loop' finishes to keep code synchronous
-  drinkId()
-})
+//'Search by id' API URL. Response contains name, pic, measurements, instructions, and ingredients
+let drinkDetailsURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 
 //This function makes an Ajax call for each drink name added into the HTML
@@ -157,10 +133,10 @@ function drinkId() {
 
 
 
-
-//multiple ingredients function
+/////////////////////////Function to parse the multiple ingredients////////////////////
 let parseIngredients = (function (drink, drinkPosition) {
 
+  $('#drinkIngredients' + drinkPosition).append('<li><strong>Ingredients:</strong></li>');
 
   for (let i = 1; i <= 15; i++) {
 
@@ -172,8 +148,78 @@ let parseIngredients = (function (drink, drinkPosition) {
   }
 })
 
-// let getRandom = function (maxNumber) {
-//   return Math.floor(Math.random() * maxNumber);
-// };
 
+
+////////////////////////conditional based on the weather//////////////////////////
+let weatherBasedIngredient = (function (currentTemp) {
+
+  let chosenIngredient;
+  //variable to hold the array of warm weather drinks: Champagne  / Grapefruit juice / Pineapple juice / Strawberries / Mango / Kiwi / Lemonade / Pisco
+  let warmWeatherIngredients = ['Champagne', 'Grapefruit%20juice', 'Pineapple%20juice', 'Cranberry%20juice', 'Lemon', 'Orange%20juice', 'Lemonade', 'Ice'];
+
+  //variable to hold the array of cold weather drinks: Coffee / Coffee liqueur / Hot Chocolate / Peppermint schnapps / Bailey's irish cream / Heavy cream / Egg / Egg Yolk
+  let coldWeatherIngredients = ['Coffee', 'Chambord%20raspberry%20liqueur', 'Jägermeister', 'Bailey%27s%20irish%20cream', 'Creme%20de%20Cacao', 'Kahlua'];
+
+
+  //if the temperature is above 65 degrees Farenheit then we will only pull drinks with the warmWeatherIngredients
+  if (currentTemp >= 65) {
+    let var1 = getRandom(warmWeatherIngredients.length);
+    chosenIngredient = warmWeatherIngredients[var1];
+  }
+  //else (it must be less than 65 degrees Farenheit outside) only show cocktails with the coldWeatherIngredients:
+  else {
+    let var1 = getRandom(coldWeatherIngredients.length);
+    chosenIngredient = coldWeatherIngredients[var1];
+  }
+
+  //return the chosen ingredient
+  return chosenIngredient;
+
+
+
+});
+
+
+
+///////////////function that loads the drinks based on the weather ///////////////////
+////////TODO: this will need to be added into our weather function once the search feature is enabled///////
+
+let loadDrinks = (function (currentTemp) {
+
+
+  let randomIngredient = weatherBasedIngredient(currentTemp);
+
+
+  //Search currently set to to global variable 'ingredient' determined by weather temperature.
+  let drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + randomIngredient;
+
+
+
+  //Ajax call to fetch a list of drinks based on ingredient
+  $.ajax({
+    url: drinkURL,
+    method: 'GET'
+  }).then(function (response) {
+
+
+    for (var i = 0; i <= 5; i++) {
+      //Generates a random number on each loop and chooses a drink
+      let randomDrink = getRandom(response.drinks.length);
+      let currentDrink = response.drinks[randomDrink];
+
+
+      //Appends the current drink in to the corresponding span pre-set in HTML.
+      //Assigns a data-attr 'id' to be used as a unique identifier for second Ajax call.
+      $('#drinkName' + i).html(currentDrink.strDrink);
+      $('#drinkName' + i).attr('data-id', currentDrink.idDrink)
+
+    }
+    //Calling this function after the above 'for loop' finishes to keep code synchronous
+    drinkId()
+  })
+
+
+});
+
+loadDrinks(95);
 
